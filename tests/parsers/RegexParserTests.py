88ce9a2ba5e66b273e65a59473f1ccc0fcfae1ef
Marc Suñé <marc.sune@i2cat.net>
2012-01-11 17:57:42 +0100

    Fix. some bugs in RegexParser, comments and actions

--- tests/parsers/RegexParserTests.py
+++ tests/parsers/RegexParserTests.py
@@ -14,21 +14,21 @@ def parseDumpAndCraft(s):
 
 print "\n\nCOLLECTIONS:"
 #collections
-parseDumpAndCraft(" if  A in collection {2,3,4}  then accept do C # dd")
-parseDumpAndCraft(" if  not A in collection {2,3,4}  then accept do C # dd")
+parseDumpAndCraft(" if  A in collection {2,3,4}  then accept ")
+parseDumpAndCraft(" if  not A in collection {2,3,4}  then deny do C # dd")
 parseDumpAndCraft(" if  A not in collection {2,3,4}  then accept do C denyMessage ksdfkdfskf  # dd")
 
 print "\n\nRANGE NO STRICT:"
 #range no strict
 parseDumpAndCraft(" if   A in range [2,4]  then accept")
 parseDumpAndCraft(" if  not A in range [2,4]  then accept do C # dd")
-parseDumpAndCraft(" if   A not in range [2,4]  then accept do C # dd")
+parseDumpAndCraft(" if   A not in range [2,4]  then deny do C # dd")
 
 print "\n\nRANGE  STRICT:"
 #range no strict
-parseDumpAndCraft(" if   A in range {2,4}  then accept terminal do C # dd")
-parseDumpAndCraft(" if  not A in range {2,4}  then accept terminal do C # dd")
-parseDumpAndCraft(" if   A not in range {2,4}  then accept terminal do C # dd")
+parseDumpAndCraft(" if   A in range {2,4}  then accept do C # dd")
+parseDumpAndCraft(" if  not A in range {2,4}  then accept do C # dd")
+parseDumpAndCraft(" if   A not in range {2,4}  then accept do C # dd")
 
 
 print "\n\nSIMPLE :"
@@ -39,6 +39,6 @@ parseDumpAndCraft(" if ( not a>5 ) then accept do something denyMessage ksdfkdfs
 print "\n\nCOMPLEX:"
 #complex
 parseDumpAndCraft(" if  ( (a>5) && (B = 5)) && ((a<4)&&(b!=5))   then accept nonterminal do something denyMessage ksdfkdfskf # comment")
-parseDumpAndCraft(" if   ( (( (a>5) && (B = 5)) || (a=b) ) && ((a<4)&&(b!=5)))   then accept do something denyMessage ksdfkdfskf # comment")
+parseDumpAndCraft(" if   ( (( (a>5) && (B = 5)) || (a=b) ) && ((a<4)&&(b!=5)))   then deny do something denyMessage ksdfkdfskf # comment")
 parseDumpAndCraft(" if   ( (( (a>5) && (B in range [2,3])) || (a=b) ) && ((a<4)&&(b!=5)))   then accept nonterminal do something denyMessage ksdfkdfskf # comment")
 
