fb1d05edb2d719aa116794c8198d054bfe20a23c
Marc Suñé <marc.sune@i2cat.net>
2012-01-11 19:02:46 +0100

    Deleting wrong test in root dir

--- testmain.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import time
-import exceptions
-import time
-
-from src.RuleTable import RuleTable
-
-Table = RuleTable(tabla,resolverMappings,defaultParser, defaultPersistence, defaultPersistenceFlag, pType = False, uuid = None)
